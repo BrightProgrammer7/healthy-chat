c39f065b64e1fa9a91f368a347fbfd923aa76cfc
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-05-13 19:59:37 +0100

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   useEffect(() => {
     // Create a new WebSocket connection
     // const newSocket = io("http://localhost:4000");
-    const newSocket = io("https://stress-server.onrender.com") || io("http://localhost:4000");
+    const newSocket = io("http://localhost:4000") || io("https://stress-server.onrender.com");
 
     // Save the WebSocket connection to state
     setSocket(newSocket);
